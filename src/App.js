a2638cc08be863c2bc03e6f93fff9ce07113aad7
rmit-s3626050-Trung-Pham <s3626050@student.rmit.edu.au>
2020-05-02 16:24:30 +1000

    the first deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          this is my first deployment
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is my first deployment
+          Learn React
         </a>
       </header>
     </div>
