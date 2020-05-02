3b29bdd9445feb8f1b5bc3da5919d73522efaafa
rmit-s3626050-Trung-Pham <s3626050@student.rmit.edu.au>
2020-05-02 16:30:51 +1000

    first text from tonybranch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is my first deployment
+          changed text in tonybranch
         </p>
         <a
           className="App-link"
