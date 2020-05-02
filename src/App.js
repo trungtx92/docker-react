848111c168d5d3e10b1de6457492607f9f2ac4dc
rmit-s3626050-Trung-Pham <s3626050@student.rmit.edu.au>
2020-05-02 16:31:58 +1000

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          changed text in tonybranch
+          changed text in tonybranch for first
         </p>
         <a
           className="App-link"
